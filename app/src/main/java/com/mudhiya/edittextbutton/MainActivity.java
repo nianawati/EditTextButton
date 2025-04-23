fd6bd7ab344857443c6531192490b3efe3bc1f28
nianawati <mudhiyanianawati55754@gmail.com>
2025-04-23 13:46:53 +0700

    EditTextButton

---
+++ app/src/main/java/com/mudhiya/edittextbutton/MainActivity.java
@@ -0,0 +1,42 @@
+package com.mudhiya.edittextbutton;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button okButton = (Button)findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText namaEditText = findViewById(R.id.namaEditText);
+                TextView infoTextView = findViewById(R.id.infoTextView);
+
+                String nama = namaEditText.getText().toString();
+                String pesan = "Hello, " + nama + "!";
+
+                infoTextView.setText(pesan);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
